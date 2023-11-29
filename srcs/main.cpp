ba26a2e64bd71004389d6ff644084b4623650cb1
Namonay <thek7995@gmail.com>
2023-11-29 14:03:49 +0100

    caca 1

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,17 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
+/*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 10:40:01 by maldavid          #+#    #+#             */
-/*   Updated: 2023/11/24 10:40:29 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:03:14 by vvaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char** av)
+#include "../includes/irc.hpp"
+
+unsigned long get_port(char *port)
 {
-	(void)ac;
-	(void)av;
+	if (!port)
+		return (0);
+	return (std::atoi(port));
+}
+int main(int argc, char** argv)
+{
+	int s_socket;
+	struct sockaddr_in s_data;
+
+	s_data.sin_family = AF_INET;
+	s_data.sin_addr.s_addr = INADDR_ANY;
+	s_data.sin_port = get_port(argv[1]);
+	if (argc != 3)
+		return (1);
+	(void)argv;
 
 	return 0;
 }
