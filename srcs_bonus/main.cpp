5de6e3a09fea7a10dbdfb8afa9361df422af3b84
Kbz-8 <kbz_8.dev@akel-engine.com>
2024-01-30 18:09:47 +0100

    caca

--- srcs_bonus/main.cpp
+++ srcs_bonus/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vvaas <vvaas@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 20:36:23 by vvaas             #+#    #+#             */
-/*   Updated: 2024/01/30 02:40:04 by vvaas            ###   ########.fr       */
+/*   Updated: 2024/01/30 17:56:37 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -15,7 +15,7 @@
 int main()
 {
 	bot greg;
-
 	greg.init();
 	greg.connect_to_server();
+	return 0;
 }
